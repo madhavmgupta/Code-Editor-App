efc733ff500fa296f29c4bba01b561686e3de9de
madhavmgupta <madhavmgupta@gmail.com>
2023-12-25 19:47:06 +0530

    Updates

---
+++ precache-manifest.c2971d9268c238e61dfa24719ab14f8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecfabbaa8ac1870cbd028e3457ff3707",
+    "url": "/Code-Editor-App/index.html"
+  },
+  {
+    "revision": "70c3ff1a8dd6beff5ca9",
+    "url": "/Code-Editor-App/static/css/2.c46397a6.chunk.css"
+  },
+  {
+    "revision": "3f3c303069ec6eda3cd6",
+    "url": "/Code-Editor-App/static/css/main.23718815.chunk.css"
+  },
+  {
+    "revision": "70c3ff1a8dd6beff5ca9",
+    "url": "/Code-Editor-App/static/js/2.4254f81d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Code-Editor-App/static/js/2.4254f81d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f3c303069ec6eda3cd6",
+    "url": "/Code-Editor-App/static/js/main.8d583660.chunk.js"
+  },
+  {
+    "revision": "cada5b2a1cd676d924bf",
+    "url": "/Code-Editor-App/static/js/runtime-main.757d99c1.js"
+  }
+]);
